adb63074109cc86fa94dc3da3bfabb12e51f631e
Yenner Joaquín Díaz Núñez <yennerdiaz@gmail.com>
2021-12-16 12:19:15 +0100

    Filter products by user owner
     On branch master
     Changes to be committed:
    	modified:   components/products/Products.js

--- components/products/Products.js
+++ components/products/Products.js
@@ -6,17 +6,18 @@
  * @flow strict-local
  */
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { ActivityIndicator } from 'react-native';
-import { collection, getDocs, onSnapshot, query } from 'firebase/firestore';
+import { collection, getDocs, onSnapshot, query, where } from 'firebase/firestore';
 
 import { db } from '../../config/firebase';
+import { AuthenticationContext } from "../../providers/";
 
 
 export default function Products({renderView}){
+  const  { user } = useContext(AuthenticationContext);
   const [ isLoading, setIsLoading ] = useState(true); 
   const [ products, setProducts ] = useState([]);
-  const ref = collection(db, "products");
 
   function onCollectionUpdate(querySnapshopt){
     const products = [];
@@ -33,7 +34,8 @@ export default function Products({renderView}){
   }
   
   useEffect(() => {
-    const unsubscribe = onSnapshot(ref, onCollectionUpdate);
+    const q = query(collection(db, "products"), where("user", "==", user.uid));
+    const unsubscribe = onSnapshot(q, onCollectionUpdate);
   }, []);
 
   return (
