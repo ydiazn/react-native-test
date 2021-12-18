7ecdd5c412f4e2263b4d045fb560d539bab4e341
Yenner Joaquín Díaz Núñez <yennerdiaz@gmail.com>
2021-12-18 02:54:11 +0100

    Setup linking
    On branch master
     Changes to be committed:
    	modified:   navigation/RootNavigator.js
    	modified:   package.json

--- navigation/RootNavigator.js
+++ navigation/RootNavigator.js
@@ -3,17 +3,35 @@ import React, { useState, useContext, useEffect } from 'react';
 import { ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { onAuthStateChanged } from 'firebase/auth';
+import * as Linking from 'expo-linking';
 
 import  AuthStack from './AuthStack';
 import  MainNavigator from './MainNavigator.js';
 import { AuthenticationContext } from '../providers';
-
 import { auth } from '../config/firebase/';
+
+
+const prefix = Linking.createURL('/');
 
 
 export const RootNavigator = () => {
   const { user, setUser } = useContext(AuthenticationContext);
   const [isLoading, setIsLoading] = useState(true);
+
+  const linking = {
+    prefixes: [prefix],
+    config: {
+      screens: {
+        Home: {
+          screens: {
+            Products: 'products/list',
+          },
+        },
+        AddProduct: "products/add"
+      }
+    }  
+  };
+
 
   useEffect(() => {
     // onAuthStateChanged returns an unsubscriber
@@ -34,7 +52,7 @@ export const RootNavigator = () => {
   }
 
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       {user ? <MainNavigator /> : <AuthStack />}
     </NavigationContainer>
   );
