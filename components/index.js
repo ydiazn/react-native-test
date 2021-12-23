55c6f70cc7135a64c12a2d1868d9811328d6c684
Yenner Joaquín Díaz Núñez <yennerdiaz@gmail.com>
2021-12-23 01:06:02 +0100

    Register push notification via expo
     On branch master
     Changes to be committed:
    	modified:   App.js
    	new file:   components/WithNotifications.js
    	modified:   components/index.js
    	new file:   notification.js
    	modified:   package.json

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import SafeView from "./SafeView.js";
 import SearchBar from "./SearchBar.js";
+import withPushNotifications from "./WithNotifications.js";
 
-export { SafeView, SearchBar }
+export { SafeView, SearchBar, withPushNotifications }
 
