6b9a81288fef549be13837e8a456f1669a664264
Yenner Joaquín Díaz Núñez <yennerdiaz@gmail.com>
2021-12-23 01:56:51 +0100

    Send internal push notification
     Changes to be committed:
    	modified:   notification.js
    	modified:   screens/others/link.js

--- notification.js
+++ notification.js
@@ -35,5 +35,20 @@ async function registerForPushNotificationsAsync() {
 }
 
 
-export { registerForPushNotificationsAsync }
+async function sendPushNotification(expoPushToken, message) {
+  const data = { to: expoPushToken, ...message}
+  console.log(data);
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+export { registerForPushNotificationsAsync, sendPushNotification }
 
